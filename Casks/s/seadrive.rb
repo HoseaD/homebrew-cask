f8934b20d3b84135597a3aaad9e5f2413df26fcf
Hosea <104315754+HoseaD@users.noreply.github.com>
2024-06-24 17:53:47 +0200

    add Version 3.0.10 for MacOS 12.1+
    "Download 3.0 version for MacOS 12.1+, otherwise download 2.0.26 version." https://www.seafile.com/en/download/

--- Casks/s/seadrive.rb
+++ Casks/s/seadrive.rb
@@ -1,17 +1,34 @@
 cask "seadrive" do
-  version "2.0.26"
-  sha256 "e43c5711510f578be9346e09bd35df8926cd4eb980330acf2d266d7cf70aff22"
 
-  url "https://download.seadrive.org/seadrive-#{version}.dmg",
-      verified: "download.seadrive.org/"
+  on_monerey :or_newer do
+    version "3.0.10"
+    sha256 "efd129f10cb34b7349889363484670f204ab47728ce979d36661b180ce52c578"
+    
+    url "https://download.seadrive.org/seadrive-#{version}.pkg",
+    verified: "download.seadrive.org/"
+
+    livecheck do
+      url "https://www.seafile.com/en/download/"
+      regex(%r{href=.*?/seadrive[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    end
+  end
+  on_ventura :or_older do
+    version "2.0.26"
+    sha256 "e43c5711510f578be9346e09bd35df8926cd4eb980330acf2d266d7cf70aff22"
+    
+    url "https://download.seadrive.org/seadrive-#{version}.dmg",
+    verified: "download.seadrive.org/"
+      
+    livecheck do
+      url "https://www.seafile.com/en/download/"
+      regex(%r{href=.*?/seadrive[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    end
+  end
+  
+
   name "Seadrive"
   desc "Manual for Seafile server"
   homepage "https://www.seafile.com/en/home/"
-
-  livecheck do
-    url "https://www.seafile.com/en/download/"
-    regex(%r{href=.*?/seadrive[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-  end
 
   depends_on macos: ">= :high_sierra"
 
